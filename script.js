b8414dd56c371ac6327ed641f1aa9a6859d49012
Tushar Sharma <sharma@aclanyc.com>
2017-03-24 19:16:40 +0000

    make

--- script.js
+++ script.js
@@ -1,18 +1,95 @@
 TinyTurtle.apply(window, [undefined, 400, 400]);
+function triangle(size){
+forward(size);
+right(120);
+forward(size);
+right(120);
+forward(size);
+right(120);
+}
+triangle(60);
 
-function square(){
-forward(50);
+function rectangle(size){
+forward(160);
 right(90);
-forward(50);
+forward(size);
 right(90);
-forward(50);
+forward(160);
 right(90);
-forward(50);
+forward(size);
 right(90);
-forward(50);
 }
 
-// Type your function call below
+
+rectangle(70);
 
 
-stamp();
+function rhombus(size){
+    forward(size);
+left(120);
+forward(size);
+left(120);
+forward(size);
+left(120);
+forward(size);
+right(120);
+forward(size);
+right(120);
+forward(size);
+right(120);
+}
+
+rhombus(-110);
+
+function square(size){
+forward(size);
+left(90);
+forward(size);
+left(90);
+forward(size);
+left(90);
+forward(size);
+left(90);
+}
+square(90);
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
